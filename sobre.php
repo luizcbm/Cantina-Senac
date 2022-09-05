5ce292a70dc10735b3ed93fb954a375e0fcef1e6
Gabriel <gabsoueu01@gmail.com>
2022-09-05 15:32:49 -0300

    pagina sobre

--- sobre.php
+++ sobre.php
@@ -1,12 +1,26 @@
 <?php
 include "head.php";
 ?>
+<div class="sobre">
+    <h1> Cantina Senac</h1>
 
+    <p>
+        A CantinaSenac visa, superar as expectativas de seus clientes com a qualidade das refeições que serve.
+        A busca da excelência na qualidade da alimentação das pessoas é a nossa prioriade.
+        E nós da turma TII2201, decidimos ajudar a equipe do Ádamo a ter uma forma de atendimento com mais rapidez para os consumidores do Senac.
 
-<div class="container">
-    <div class="w-75 bg-gray mx-auto">
-        <h1>Oi somos a melhor cantina</h1>
-    </div>
+    </p>
+</div>
+    <img src="site/images/senacfrente.jpg" >
+<div class="equipe">
+    <h1> Equipe </h1>
+    <ul>
+        <li>Leandro Lobo</li>
+        <li>Luiz Machado</li>
+        <li>Gabriel Dias</li>
+        <li>Jalison Santos</li>
+        <li>João Gabriel</li>
+    </ul>
 </div>
 
 
