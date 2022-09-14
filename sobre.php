b0b8f7dd247873be8cba10525156e3a2aa45b7d4
luiz carlos <luiz.machado3@alunos.sc.senac.br>
2022-09-14 16:55:29 -0300

    sai daqui joão

--- sobre.php
+++ sobre.php
@@ -1,21 +1,19 @@
-<?php
-include_once " head.php ";
-?>
+
 
 <?php
  include "nav.php";
  ?>
 <div  style =" background:#F7F7F5 ; margin-top: 20px; " class =" container " >
     <div  class =" w-75 bg-gray mx-auto " >
-        <h1  style =" color: black ; " > Conheça um pouco mais sobre o Ádamo Cozinha Brasileira </h1>
-        <h5  style =" color: black ; " > O Ádamo Cozinha Brasileira procura superar as expectativas de seus clientes com a qualidade das refeições que servem. A busca da excelência na qualidade da alimentação das crianças é prioritária.
-  Além de servir, todos os dias, uma alimentação saudável e saborosa, o Ádamo Cozinha Brasileira pelo desenvolvimento de ações para estimular uma boa educação alimentar e nutricional.
-Todas as pessoas como pessoas saudáveis ​e preservadas para uma nutrição adequada. Para isso, além do acesso aos alimentos, é essencial a construção de hábitos alimentares. </h5 >
+        <h1  style =" color: black ; " > Conheça um pouco mais sobre o Cantina senac</h1>
+        <h5  style =" color: black ; " >Conheça um pouco da nossa história. No dia 08\02\2022 iniciamos nossa trajetoria como programadores,
+         pouco tempo depois iniciamos esse projeto, no qual com muito trabalho, hoje temos um site bem construido para matar sua fome.</h5 >
         <h2  style =" color: black ; " > Cardápios Especiais: </h2 >
         <h5  style= "color : black ;" > Elaboração de cardápios de cores diferenciados em atendimento a diversas patologias, como diabetes tais: intolerância ao glúten.
-            O nosso restaurante oferece cardápios temáticos mensalmente, entre eles estão, havaiano, americano, alemão, oriental, junino, italiano, entre outros. </h5>
+            O nosso restaurante oferece cardápios temáticos mensalmente, entre eles estão: havaiano, americano, alemão, oriental, junino, italiano, entre outros. </h5>
 
     </div >
 </div >
+<style>
 
 
