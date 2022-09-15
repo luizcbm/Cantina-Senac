622110d8755a3ff8085a4f013bd11ece1b3a25ae
JALISON LUIZ FERREIRA SANTOS <jalison.santos@alunos.sc.senac.br>
2022-09-15 16:54:38 -0300

    passei meus arquivos da minha outra pasta cantina-senac-jalison

--- bebidas.php
+++ bebidas.php
@@ -1,29 +1,31 @@
 <?php
-include "site/include/MySql.php";
+//include "head.php";
+include "include/MySql.php";
 
 
 
-$sql = $pdo->prepare('SELECT * FROM produtos WHERE codigo LIKE "11" ');
+$sql = $pdo->prepare('SELECT * FROM produtos ');
 
 if ($sql->execute()) {
     $info = $sql->fetchAll(PDO::FETCH_ASSOC);
-    
+    echo "<div id='cardapio' class='flex'>"; 
     foreach ($info as $key => $value) {
-
-
-                                                                        
-
-    $imagem = $value["imagem"];
-
-    echo '<img style= "width:300px;"src="data:image/jpg;charset=utf8;base64,' . base64_encode($imagem) . '"></td>';
-    
-    echo '<h2>'.$value["nome"].'</h2>';
-
-    echo "</div>";  
-
+        $imagem = $value["imagem"];
+ 
+            echo "<div id='item'>";
+                echo "<h1>".$value["nome"]."</h1>";
+                echo "<img id='imagens' style= 'width:300px;'src='data:image/jpg;charset=utf8;base64," . base64_encode($imagem) . "'></td>";
+                echo "<p>".$value['descricao']."</p>";
+                echo "<h1>".$value['valor']."</h1>";
+                echo "<button>Comprar</button>";
+            echo "</div>";
+        
     }
+    echo "</div>";
 
 }
 ?>
- <link rel="stylesheet" href="site/css/refri.css">
-
+<link rel="stylesheet" href="assets/css/refri.css">
+<?php
+include "footer.php"
+?>
