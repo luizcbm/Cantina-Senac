a4c25dbe5a79e0821d33baa028ed94a77064b4bd
JALISON LUIZ FERREIRA SANTOS <jalison.santos@alunos.sc.senac.br>
2022-09-21 14:59:01 -0300

    consertei a delCategoria e revisei alguns codigos

--- delCategoria.php
+++ delCategoria.php
@@ -2,13 +2,13 @@
     include "include/MySql.php";
 
     $msgErro = "";
-
+$codCategoria ="";
 
     if (isset($_GET['id'])){
-        $codigo = $_GET['id'];
+        $codCategoria = $_GET['id'];
 
-        $sql = $pdo->prepare("DELETE FROM categoria = ?");
-        if ($sql->execute(array($codigo))){
+        $sql = $pdo->prepare("DELETE FROM categoria WHERE codCategoria = ?");
+        if ($sql->execute(array($codCategoria))){
             if ($sql->rowCount() > 0){
                 $msgErro = "produto excluído com sucesso!";
                 header('location:listCategoria.php');
